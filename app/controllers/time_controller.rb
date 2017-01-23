4aba4b527f66dee3f812c19e39b2eea22a7d84af
leoniknik <your_email@whatever.com>
2017-01-24 01:39:14 +0300

    Add download photo

--- app/controllers/time_controller.rb
+++ app/controllers/time_controller.rb
@@ -1,5 +1,10 @@
 class TimeController < ApplicationController
+  before_filter :authenticate_user!
   def show
     @time = Time.new
+  end
+
+  def test
+    @one = rand(10)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   resources :ideas
   devise_for :users
   get 'time/show'
-  root 'time#show'
+  get 'time/test'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
